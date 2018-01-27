6d6150ed0ff673835f08789eb698a4317ff21dad
Toodyao-XPS-8500 <toodyao@gmail.com>
2018-01-27 21:46:24 +0800

    fix indentation

--- L1-011.cpp
+++ L1-011.cpp
@@ -10,10 +10,10 @@ int main()
 	int b[129] = { 0 };
 	getline(cin, a);
 	while ((ch = getchar()) != '\n')
-	    b[ch] = 1;
+		b[ch] = 1;
 	for (int i = 0; i < a.length(); i++)
-	    if (!b[a[i]])
-		  cout << a[i];
+		if (!b[a[i]])
+			cout << a[i];
 
 	return 0;
 }
