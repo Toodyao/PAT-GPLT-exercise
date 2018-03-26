bd24072931ff0aa442a1705f6fe507be640dc576
Toodyao-XPS-13 <toodyao@gmail.com>
2018-03-26 21:03:23 +0800

    optimize dfs()

--- L2-020.cpp
+++ L2-020.cpp
@@ -11,19 +11,13 @@ int good[100010];
 double sum;
 
 void dfs(int n) {
-	if (good[n] == 1)
+	if (good[n] == 1) {
 		sum += power[n] * v[n][0];
-	for (size_t i = 0; i < v[n].size(); i++) {
-		// printf("%d\n", v[n][i]);
-		if (good[v[n][i]] == 1) {
-			power[v[n][i]] = power[n] * v[v[n][i]][0] * (1.0 - r / 100.0);
-			// printf("%d %lf %d\n", n, power[n] , v[v[n][i]][0]);
-			sum += power[v[n][i]];
-			continue;
-		} else {
-			power[v[n][i]] = power[n] * (1.0 - r / 100.0);
-			dfs(v[n][i]);
-		}
+		return;
+	}
+	for (int i = 0; i < v[n].size(); i++) {
+		power[v[n][i]] = power[n] * (1.0 - r / 100.0);
+		dfs(v[n][i]);
 	}
 }
 
@@ -46,16 +40,6 @@ int main() {
 			}
 		}
 	}
-	// for (int i = 0; i < n; i++) {
-	// 	for (size_t ii = 0; ii < v[i].size(); ii++) {
-	// 		cout << v[i][ii] << " ";
-	// 	}
-	// 	cout << endl;
-	// }
-	// for (int i = 0; i < n; i++) {
-	// 	printf("%d ", good[i]);
-	// }
-	// printf("\n");
 	power[0] = z;
 	dfs(0);
 	cout << (int)sum;
